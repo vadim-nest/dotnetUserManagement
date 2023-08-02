57022924faaa6e337129c474ff8f832cf06f2884
Vadim Gnezdyshkin <vadim@gne.me.uk>
2023-08-02 13:54:57 +0100

    [Feature] Add filter functionality

--- UserManagement.Web/wwwroot/js/site.js
+++ UserManagement.Web/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $("#btnActiveOnly").on("click", function () {
+        filterUsers(true);
+    });
+
+    $("#btnNonActive").on("click", function () {
+        filterUsers(false);
+    });
+
+    function filterUsers(isActive) {
+        $(".table tbody tr").hide();
+
+        // Show only the rows that match the isActive condition
+        $(".table tbody tr").each(function () {
+            const isActiveUser =
+                $(this).find("td:nth-child(5)").text().trim() === "Yes";
+            if ((isActive && isActiveUser) || (!isActive && !isActiveUser)) {
+                $(this).show();
+            }
+        });
+    }
+});
